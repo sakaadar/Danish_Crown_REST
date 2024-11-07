c93a25c9e89f64d36ea14f0b14173e72ae15bb8a
Sakaadar <79919443+Sakaadar@users.noreply.github.com>
2024-11-07 11:24:18 +0100

    Initial commit

---
+++ src/main/java/via/pro3/danish_crown_rest/DanishCrownRestApplication.java
@@ -0,0 +1,13 @@
+package via.pro3.danish_crown_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanishCrownRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanishCrownRestApplication.class, args);
+	}
+
+}
